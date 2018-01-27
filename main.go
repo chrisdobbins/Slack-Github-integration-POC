9a791b2324623279037807346c918636d3c0d340
christopher dobbins <christopherjosephdobbins@gmail.com>
2018-01-27 18:05:02 -0500

    test push

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type CircleResp struct {
+	Payload Payload `json:"payload"`
+}
+
+type Payload struct {
+	VCS_URL  string `json:"vcs_url"`
+	BuildURL string `json:"build_url"`
+	BuildNum string `json:"build_num"`
+	Branch   string `json:"branch"`
+}
 
 func main() {
 	fmt.Println("hello")
@@ -16,7 +28,14 @@ func main() {
 			fmt.Println("bad request")
 			return
 		}
+		notification := CircleResp{}
 		fmt.Println(fmt.Sprintf("%+v", body))
+		err = json.Unmarshal(body, notification)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(notification.Payload)
 
 	})
 	http.ListenAndServe(":8080", nil)
