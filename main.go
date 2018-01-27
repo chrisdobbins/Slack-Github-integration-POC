1488b8a4b86572bfc7e91ec920baa3bda4cdc404
christopher dobbins <christopherjosephdobbins@gmail.com>
2018-01-27 17:56:19 -0500

    updating

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,6 +10,14 @@ func main() {
 	fmt.Println("hello")
 	http.HandleFunc("/circlewebhook", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Success!")
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			fmt.Println("bad request")
+			return
+		}
+		fmt.Println(fmt.Sprintf("%+v", body))
+
 	})
 	http.ListenAndServe(":8080", nil)
 }
