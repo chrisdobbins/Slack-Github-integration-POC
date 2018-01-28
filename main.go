62d6a70d2f6975ed6675c4e0da418743d1bdaf19
christopher dobbins <christopherjosephdobbins@gmail.com>
2018-01-27 19:32:00 -0500

    adding Slack stuff

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 type CircleResp struct {
@@ -16,10 +18,37 @@ type Payload struct {
 	BuildURL string `json:"build_url"`
 	BuildNum int    `json:"build_num"`
 	Branch   string `json:"branch"`
+	Outcome  string `json:"outcome"`
+}
+
+type GHEvent struct {
+	Release Release `json:"release"`
+}
+
+type Release struct {
+	TagName string `json:"tag_name"`
 }
 
 func main() {
-	fmt.Println("hello")
+	appUrl := os.Getenv("SLACK_URL")
+
+	http.HandleFunc("/githubwebhook", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Success!")
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			fmt.Println("bad request")
+			return
+		}
+		ghEvt := GHEvent{}
+		err = json.Unmarshal(body, &ghEvt)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		fmt.Println(fmt.Sprintf("%+v", ghEvt))
+		http.Post(appUrl, "application/json", bytes.NewBuffer([]byte(`{"text": "testing, testing..."}`)))
+	})
 	http.HandleFunc("/circlewebhook", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Success!")
 		body, err := ioutil.ReadAll(r.Body)
@@ -29,7 +58,6 @@ func main() {
 			return
 		}
 		notification := CircleResp{}
-		fmt.Println(fmt.Sprintf("%+v", body))
 		err = json.Unmarshal(body, &notification)
 		if err != nil {
 			fmt.Println(err.Error())
